a5b52fe569f8e9a2b9069a49c49c0eee9e34f255
Sergey Andreev <s.andreev@pixelplex.io>
2025-07-18 14:44:55 +0300

    fix(TAC-70): fix REST API port

--- contrib/localnet/start.sh
+++ contrib/localnet/start.sh
@@ -4,6 +4,8 @@ CHAIN_ID=${CHAIN_ID:-tacchain_2391-1}
 TACCHAIND=${TACCHAIND:-$(which tacchaind)}
 HOMEDIR=${HOMEDIR:-$HOME/.tacchaind}
 
+sed -i.bak -E 's@(address = "tcp://)[^:/]+(:[0-9]+")@\10.0.0.0\2@g' "$HOMEDIR/config/app.toml"
+
 $TACCHAIND start --chain-id $CHAIN_ID \
   --home $HOMEDIR \
   --p2p.laddr $P2P_LADDR \
